7fe39fd149624619263047d8c5d8b70f0ff3e4b1
manuel3cv51 <94109277+manuel3cv51@users.noreply.github.com>
2021-11-16 01:52:19 -0600

    Subida de archivos

---
+++ README.md
@@ -0,0 +1,2 @@
+# bewp3cv51.github.io
+Ejemplo de autenticación con Firebase y Google.
---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyD_w7tElrRY_w11qeihIEh5qmQ0uAvYvhc",
+  authDomain: "fbautenz.firebaseapp.com",
+  projectId: "fbautenz",
+  storageBucket: "fbautenz.appspot.com",
+  messagingSenderId: "71744563029",
+  appId: "1:71744563029:web:2ffbf59088bb7f9a30f1dc",
+  measurementId: "G-7H71EFJ64N"
+    });
